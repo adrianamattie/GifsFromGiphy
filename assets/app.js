a722ab8d221ee2631d9499213c576ee9ccbd0411
Adriana Shulman <adriana33@mac.com>
2018-05-20 00:23:18 -0400

    update

--- assets/app.js
+++ assets/app.js
@@ -37,10 +37,56 @@ $(document).ready(function () {
 
 
     $("#addBtn").click(function () {
-        event.preventDefault();
-        buttonName = $("#country-input").val().trim();
-        console.log(buttonName);
-         addButtons();
+        if (buttonName == "") {
+            break;
+            console.log("Nothing was entered")
+        } else {
+            event.preventDefault();
+            buttonName = $("#country-input").val().trim();
+            console.log(buttonName);
+            addButtons();
+        }
     });
 
+
+
 });
+
+function showGifs() {
+
+    var country = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        person + "&api_key=dc6zaTOxFJmzC&limit=20";
+    // saying grab 20 gifs
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function (response) {
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div class='item'>");
+
+                var rating = results[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+
+                var countryImage = $("<img>");
+                countryImage.attr("src", results[i].images.fixed_height.url);
+
+                gifDiv.prepend(p);
+                gifDiv.prepend(countryImage);
+
+                $("#gifs-appear-here").prepend(gifDiv);
+                console.log(results[i].images.fixed_height.url);
+            }
+        });
+};
+
+  $("button").on("click", function () {
+       event.preventDefault();
+      alert("Hello");
+   showGifs();
+  });
+
